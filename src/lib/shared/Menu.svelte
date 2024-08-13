edb85dc507585a38ddd5653dc5764313878c5f25
Kino <faiza.fehri+github@gmail.com>
2024-08-13 11:26:09 -0400

    feat:nav-bar

--- src/lib/shared/Menu.svelte
+++ src/lib/shared/Menu.svelte
@@ -1,3 +1,32 @@
-<div class="flex flex-row justify-center bg-blue-400 p-10">
-    Menu
+<script>
+	import menuicon from '$lib/assets/icone_menu.svg';
+	import mysticlogo from '$lib/assets/logo_blanc.png';
+	import musicicon from '$lib/assets/icone_musique.svg';
+	import dragonicon from '$lib/assets/icone_dragon.svg';
+</script>
+
+<div class="flex flex-row justify-evenly absolute items-center mx-20 z-30">
+	<div class="flex gap-5">
+		<img
+			src={dragonicon}
+			alt="icone_dragon"
+			width="70"
+			class="hover:scale-110 active:scale-90 transition-all ease-in-out duration-200"
+		/>
+		<img
+			src={musicicon}
+			alt="icone_musique"
+			width="70"
+			class="hover:scale-110 active:scale-90 transition-all ease-in-out duration-200"
+		/>
+	</div>
+	<img src={mysticlogo} alt="logo" width="20%" class="mr-20" />
+	<div class="flex">
+		<img
+			src={menuicon}
+			alt="icone_menu"
+			width="70"
+			class="hover:scale-110 transition-all ease-in-out duration-300"
+		/>
+	</div>
 </div>
